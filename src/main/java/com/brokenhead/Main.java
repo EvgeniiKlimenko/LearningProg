d1be1599d7b28ee1a79d49ec34def3bc0113bb08
Evgenii_Klimenko <Evgenii.Klimenko@syngenta.com>
2024-03-07 16:21:01 +0400

    Added simple sort: bubble and select sortings

--- src/main/java/com/brokenhead/Main.java
+++ src/main/java/com/brokenhead/Main.java
@@ -1,5 +1,6 @@
 package com.brokenhead;
 
+import com.brokenhead.algorithms.SimpleSort;
 import com.brokenhead.patterns.adapter.Bird;
 import com.brokenhead.patterns.adapter.Drone;
 import com.brokenhead.patterns.adapter.DroneAdapter;
@@ -17,28 +18,32 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        System.out.println("Strategy pattern example:\n");
-        Duck mallardDuck = new MallardDuck();
-        Duck rubber = new RubberDuck();
-        mallardDuck.display();
-        mallardDuck.performQuacking();
-        mallardDuck.swim();
-        rubber.display();
-        rubber.performQuacking();
-        rubber.swim();
+//        System.out.println("Strategy pattern example:\n");
+//        Duck mallardDuck = new MallardDuck();
+//        Duck rubber = new RubberDuck();
+//        mallardDuck.display();
+//        mallardDuck.performQuacking();
+//        mallardDuck.swim();
+//        rubber.display();
+//        rubber.performQuacking();
+//        rubber.swim();
+//
+//        System.out.println("Adapter pattern example:\n");
+//        Drone testDrone = new DroneTaxi();
+//        Bird adapter = new DroneAdapter(testDrone); // now we can use Bird interface for a drone and pass it as a Bird
+//        adapter.fly();
+//        adapter.sing();
+//
+//        System.out.println("Observer pattern example:\n");
+//        PublishingHouse igromania = new PublishingHouse();
+//        Observer readerOne = new MagazineReader();
+//        readerOne.subscribeTo(igromania);
+//        igromania.setImportantValue(150); // notifySubscribers() is triggered here
 
-        System.out.println("Adapter pattern example:\n");
-        Drone testDrone = new DroneTaxi();
-        Bird adapter = new DroneAdapter(testDrone); // now we can use Bird interface for a drone and pass it as a Bird
-        adapter.fly();
-        adapter.sing();
-
-        System.out.println("Observer pattern example:\n");
-        PublishingHouse igromania = new PublishingHouse();
-        Observer readerOne = new MagazineReader();
-        readerOne.subscribeTo(igromania);
-        igromania.setImportantValue(150); // notifySubscribers() is triggered here
-
-
+        System.out.println("Simple sorting:\n");
+        int[] array = {55, 88, 33, 77, 11, 22, 99, 44, 555, 111};
+        SimpleSort simpleSort = new SimpleSort();
+        //simpleSort.bubbleSort(array);
+        simpleSort.selectSort(array);
     }
 }
--- src/main/java/com/brokenhead/algorithms/SimpleSort.java
+++ src/main/java/com/brokenhead/algorithms/SimpleSort.java
@@ -1,13 +1,46 @@
 package com.brokenhead.algorithms;
 
+import java.util.Arrays;
+
 public class SimpleSort {
 
+    /**
+     * Bigger element goes right(upper position)
+     * Working with pairs of numbers.
+     * Inner loop - from the first element to outer-1
+     */
     public void bubbleSort(int[] array) {
-
+        System.out.println("Bubble sort. Before: " + Arrays.toString(array));
+        int outer, inner;
+        for (outer = (array.length - 1); outer > 1; outer--) { // last iteration is outer=2
+            for (inner = 0; inner < outer; inner++) {    // last iteration is inner=1 (pair 1 and 2)
+                if (array[inner] > array[inner + 1]) {
+                    int temp = array[inner];
+                    array[inner] = array[inner + 1];
+                    array[inner + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Bubble sort. After: " + Arrays.toString(array));
     }
 
     public void selectSort(int[] array) {
+        System.out.println("Select sort. Before: " + Arrays.toString(array));
+        int outer, inner, min;
+        for (outer = 0; outer < (array.length - 1); outer++) { // last iteration is last-1(pre-last element)
+            min = outer;
+            // inner loop to find min element position only
+            for (inner = (outer + 1); inner < array.length; inner++) { // from outer+1 to the end, check all.
+                if (array[inner] < array[min]) {
+                    min = inner;
+                }
+            }
+            int temp = array[outer]; // swap old min value with new min value
+            array[outer] = array[min];
+            array[min] = temp;
 
+        }
+        System.out.println("Select sort. After: " + Arrays.toString(array));
     }
 
     public void insertSort(int[] array) {
